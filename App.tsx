64a29225650db51ddefd40361afa79f8b2a93248
Nitesh <niteshreddy257@gamil.com>
2023-05-19 16:20:39 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text>Nitesh</Text>
+    </View>
+  )
+}
+
+export default App;
