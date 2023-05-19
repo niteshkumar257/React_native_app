49ec4a95a490c7a4d0703ab58deac26893a053dd
Nitesh <niteshreddy257@gamil.com>
2023-05-19 20:51:11 +0530

    waste-commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
-import { View, Text } from 'react-native'
+import { View, Text ,StyleSheet} from 'react-native'
 import React from 'react'
+import { create } from 'react-test-renderer'
+
 
 const App = () => {
   return (
     <View>
-      <Text>Nitesh</Text>
+     
     </View>
   )
 }
+const styles=StyleSheet.create({
+   container:{
+       display:"flex",
+       flexDirection:"column",
+       height:100,
+       backgroundColor:"red"
+   }
+})
 
 export default App;
