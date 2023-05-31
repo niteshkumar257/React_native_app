3894a6ba3e9da48b9442a350115e740ce689d036
Nitesh <niteshreddy257@gamil.com>
2023-05-31 14:07:53 +0530

    package-namechanged

--- ios/GwParentTests/GwParentTests.m
+++ ios/GwParentTests/GwParentTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AwesomeProjectTests : XCTestCase
+@interface GwParentTests : XCTestCase
 
 @end
 
-@implementation AwesomeProjectTests
+@implementation GwParentTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
