d732b9df78af36d95c0a816e1734796b16837ab3
Nitesh <niteshreddy257@gamil.com>
2023-07-21 03:10:23 +0530

    parent done

--- Components/bottom/Curriculum/Curriculum.js
+++ Components/bottom/Curriculum/Curriculum.js
@@ -1,150 +1,88 @@
-import { View, Text ,StyleSheet,ScrollView,FlatList,Image} from 'react-native'
-import React ,{useEffect,useState,useContext} from 'react'
-import DataContext from '../../Context/DataContext'
-import axios from 'axios'
-import {COLORS} from "../../Utils/Colors/Colors";
-import AcitvityHandler from '../AcitvityHandler';
-import { Dimensions } from 'react-native';
-const { width, height } = Dimensions.get('window');
-import NotFoundText from '../NotFoundText';
+import React, { useContext,useState,useEffect } from 'react';
+import { View, Text, StyleSheet, ScrollView, Image, Dimensions } from 'react-native';
 import { useQuery } from '@tanstack/react-query';
-import { GW_URL } from '../../config';
 import Pinchable from 'react-native-pinchable';
+import DataContext from '../../Context/DataContext';
+import axios from 'axios';
+import { COLORS } from "../../Utils/Colors/Colors";
+import AcitvityHandler from '../AcitvityHandler';
+import { GW_URL } from '../../config';
+
+const { width, height } = Dimensions.get('window');
+
 const Screen1 = () => {
-   
-  const {id}=useContext(DataContext);
-  const [childId,setChildId]=useState(id);
- const Curriculum=(require("../../../assets/data1.png"));
- const  [schoolId,setSchoolId]=useState();
- const [classId,setClassId]=useState();
- const [url,setUrl]=useState(null);
-const [show,setShow]=useState(true);
+  const { id } = useContext(DataContext);
+  const [childId, setChildId] = useState(id);
+  const [show, setShow] = useState(true);
 
+  const { data: ClassIdAndSchoolId, isLoading: IdsLoading, isError: IdsError, error: Idserror } = useQuery({
+    queryKey: ["fetch-class_id-school_id", childId],
+    queryFn: () => {
+      return axios.get(`${GW_URL}/students/${childId}`)
+    }
+  });
 
+  const class_id = ClassIdAndSchoolId?.data?.studentDetails[0].class_id;
+  const school_id = ClassIdAndSchoolId?.data?.studentDetails[0].school_id;
 
+  const { data: res, isLoading: curriculumLoading, isError: curriculumErrorStatus, error: ErrorMsg } = useQuery({
+    queryKey: ['curriculum', class_id, school_id],
+    queryFn: () => {
+      return axios.get(`${GW_URL}/viewCurriculum`, {
+        params: {
+          school_id: school_id,
+          class_id: class_id
+        }
+      });
+    },
+    enabled: !!class_id && !!school_id
+  });
 
- const {data:ClassIdAndSchoolId,isLoading:IdsLoading,isError:IdsError,error:Idserror}=useQuery({
-   queryKey:["fetch-class_id-school_id",childId],
-   queryFn:()=>
-   {
-   return  axios.get(`${GW_URL}/students/${childId}`)
-   }
- })
- 
- const class_id=ClassIdAndSchoolId?.data?.studentDetails[0].class_id;
- const school_id=ClassIdAndSchoolId?.data?.studentDetails[0].school_id;
- const {data:res,isLoading:curriculumLoading,isError:curriculumErrorStatus,error:ErrorMsg}=useQuery({
-      queryKey:['curriculum',class_id,school_id],
-      queryFn:()=>
-      {
-        return  axios.get(`${GW_URL}/viewCurriculum`, {
-          params: {
-            school_id: school_id,
-            class_id:class_id
-            }
-         })
-      },
-      enabled:!! class_id && !!school_id
- })
- 
- 
-
-
- 
- if(curriculumErrorStatus)
- {
-  console.log(ErrorMsg.message);
- }
   return (
     <ScrollView style={styles.ViewContainer}>
-      <View style={styles.container}> 
-  {curriculumLoading && <AcitvityHandler show={curriculumLoading}/>}
-   {
-    res?.data.url!=null ?
-    <Pinchable>
-  <Image  style={styles.Image} source={ { uri: res?.data.url }}/>
-    </Pinchable>
-    :
-    <Text style={{
-      fontSize:20,
-      fontWeight:600,
-      color:"black"
-    }}>No curriculum is assigned</Text> 
-    }
-   
- 
- 
-</View>
+      <View style={styles.container}>
+        {curriculumLoading && <AcitvityHandler show={curriculumLoading} />}
+        {res?.data?.url != null ? (
+          <View style={styles.imageContainer}>
+            <Pinchable>
+              <Image style={styles.image} source={{ uri: res?.data?.url }} />
+            </Pinchable>
+          </View>
+        ) : (
+          <Text style={styles.noCurriculumText}>No curriculum is assigned</Text>
+        )}
+      </View>
     </ScrollView>
-    
-  )
-}
-const styles=StyleSheet.create(
-  {
-    ViewContainer:{
-      height:height-50,
-      width:width,
-      padding:5,
-      backgroundColor:COLORS.backgGroundColor,
+  );
+};
 
-    
-},
-    container:{
-      height:height-50,
-      width:width-20,
-       display:"flex",
-       justifyContent:"center",
-       alignItems:"center",
-     
-     
-      
-    },
-    text:{
-      fontSize:20,
-      color:"black",
-      fontWeight:600,
-     
-    },
-    contentBox:{
-      height:200,
-      display:"flex",
-      justifyContent:"center",
-      alignItems:"center",
-       
-      borderWidth:1,
-      borderColor:"black",
-    
-    
+const styles = StyleSheet.create({
+  ViewContainer: {
+    flex: 1,
+    backgroundColor: COLORS.backgGroundColor,
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imageContainer: {
+    marginVertical: 20,
+   
+   
+    overflow: "hidden",
+  },
+  image: {
+    width: width - 20,
+    height: height - 200,
+    resizeMode: "contain",
+  },
+  noCurriculumText: {
+    fontSize: 20,
+    fontWeight: "600",
+    color: "black",
+    marginTop: 20,
+  },
+});
 
-
-
-    },
-    title:{
-      height:40,
-      width:"100%",
-      padding:20,
-      margin:10,
-      backgroundColor:"grey"
-      
-
-    },
-    body:{
-      minHeight:100,
-      width:"100%",
-      padding:20,
-      margin:10,
-      backgroundColor:"lightgrey"
-
-    },
-    Image:
-
-    {
-      height:height-200,
-      width:width-20,
-      resizeMode: "contain",
-      padding:10
-    }
-
-  }
-)
-export default Screen1
+export default Screen1;
